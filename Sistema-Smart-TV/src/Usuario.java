d20704bacab42a8b077b84c6bf837b69aba69206
Daniel Freitas <danielmgdfpm@gmail.com>
2024-07-02 11:10:49 -0300

    Alterações mínimas em Sistema-Smart-TV.

--- Sistema-Smart-TV/src/Usuario.java
+++ Sistema-Smart-TV/src/Usuario.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Usuario
 {
     public static Scanner leia = new Scanner(System.in);
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args)
     {
         SmartTV smartTV = new SmartTV();
 
